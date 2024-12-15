c3acb40f60f2c4e8836bb2854d94266046ae64eb
almayomekonen <alamayo274@gmail.com>
2024-12-15 08:56:22 +0200

    paths-fixed

--- components/ui/Notification.jsx
+++ components/ui/Notification.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 
-import classes from "./notification.module.css";
+import classes from "./Notification.module.css";
 
 export default function Notification(props) {
   const { title, message, status } = props;
